dc22c770ec0c20696b205febec6e11d0bd7bbfd9
kute <kutekute00@gmail.com>
2018-07-25 10:23:15 +0800

    test

---
+++ src/main/java/com/kute/interceptor/RequestInterceptor.java
@@ -0,0 +1,23 @@
+package com.kute.interceptor;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * created by kute on 2018/07/25 10:19
+ */
+public class RequestInterceptor extends HandlerInterceptorAdapter {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        return super.preHandle(request, response, handler);
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        super.postHandle(request, response, handler, modelAndView);
+    }
+}
